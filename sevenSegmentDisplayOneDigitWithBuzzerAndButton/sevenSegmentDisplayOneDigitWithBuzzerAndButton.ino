0ff86fa63e232ea14ed94d01d31213c68f13114f
Damian Jankowski <58376516+DamiXioR@users.noreply.github.com>
2023-07-03 18:10:27 +0200

    Create sevenSegmentDisplayOneDigitWithBuzzerAndButton.ino

---
+++ sevenSegmentDisplayOneDigitWithBuzzerAndButton/sevenSegmentDisplayOneDigitWithBuzzerAndButton.ino
@@ -0,0 +1,196 @@
+/*
+ fg gnd ab
+    a
+  -----
+ |     |
+f|     |b
+ |  g  |
+  -----
+ |     |
+e|     |c
+ |     |
+  -----
+    d
+ed gnd c dp
+*/
+
+using PinType = uint8_t;
+
+PinType pinA {13};
+PinType pinB {12};
+PinType pinC {11};
+PinType pinD {10};
+PinType pinE {9};
+PinType pinF {8};
+PinType pinG {7};
+PinType pinBuzzer {4};
+
+PinType controlPin {2};
+bool controlState {LOW};
+uint8_t numPush {0};
+
+uint16_t milliSecDelay {200};
+
+void display0() {
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, HIGH);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, LOW);
+}
+
+void display1() {
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, LOW);
+}
+
+void display2() {
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, LOW);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, HIGH);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, HIGH);
+}
+
+void display3() {
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, HIGH);
+}
+
+void display4() {
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, HIGH);
+}
+
+void display5() {
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, HIGH);
+}
+
+void display6() {
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, HIGH);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, HIGH);
+}
+
+void display7() {
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, LOW);
+}
+
+void display8() {
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, HIGH);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, HIGH);
+}
+
+void display9() {
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, HIGH);
+}
+
+void displayNumber(uint16_t number) {
+  switch (number) {
+    case 0:
+      display0();
+      break;
+    case 1:
+      display1();
+      break;
+    case 2:
+      display2();
+      break;
+    case 3:
+      display3();
+      break;
+    case 4:
+      display4();
+      break;
+    case 5:
+      display5();
+      break;
+    case 6:
+      display6();
+      break;
+    case 7:
+      display7();
+      break;
+    case 8:
+      display8();
+      break;
+    case 9:
+      display9();
+      break;
+    default:
+      break;
+  }
+}
+
+void setup() {
+  pinMode(pinA, OUTPUT);
+  pinMode(pinB, OUTPUT);
+  pinMode(pinC, OUTPUT);
+  pinMode(pinD, OUTPUT);
+  pinMode(pinE, OUTPUT);
+  pinMode(pinF, OUTPUT);
+  pinMode(pinG, OUTPUT);
+  
+  pinMode(controlPin, INPUT);
+  pinMode(pinBuzzer, OUTPUT);
+}
+
+void loop() {
+  controlState = digitalRead(controlPin);
+  if (controlState == HIGH){
+    digitalWrite(pinBuzzer, HIGH);
+    numPush++;
+    if (numPush == 10){
+      numPush = 0;
+    }
+    delay(milliSecDelay);
+  }
+  displayNumber(numPush);
+  digitalWrite(pinBuzzer, LOW);
+}
